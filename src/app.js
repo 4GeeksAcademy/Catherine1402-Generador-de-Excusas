3a4c4528069e025d10e723ae146ddd30b1c45287
Catherine1402 <144625881+Catherine1402@users.noreply.github.com>
2023-10-22 22:38:05 +0000

    Escusas finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuseGenerator();
 };
+
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function random(array) {
+    let indice = Math.floor(Math.random() * array.length);
+    return array[indice];
+  }
+
+  let exc =
+    random(who) +
+    " " +
+    random(action) +
+    " " +
+    random(what) +
+    " " +
+    random(when);
+  return exc;
+}
